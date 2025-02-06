e55b08a73a9bb45facbb6340baacc6668a4b850c
Shahbaj Mukul <shahbajmukul@gmail.com>
2025-02-06 15:07:06 -0500

    Correted countdown logic and add liftoff message

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        for(int i=1;i<=10;i++) {
+        for(int i=10;i>=0;i--) {
             System.out.println("Countdown: "+i);
         }
+        System.out.println("We have liftoff!");
     }
 }
